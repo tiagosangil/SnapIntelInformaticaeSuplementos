d811bcd4aff4cbad8a3379014d2457ea42c714c0
tiagosangil <106884372+tiagosangil@users.noreply.github.com>
2025-03-19 12:29:15 -0300

    Add files via upload

---
+++ Meu PHP.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Estamos aprendendo PHP!</h1>
+        <?php
+        echo "Eae men kk?";
+        //Eu nÃ£o!Sou apenas um comentÃ¡rio.
+        echo "Oi,tudo bom?<br>";
+        #Qual é mano,beleza cara!
+        echo"Apareci aqui de novo na sua tela,kkk<br>";
+        /*Mano,tu é chato viu,some daqui meu*/
+        ?>
+        <?php
+        define("PHP","Linguagem Open-Source");
+        echoPHP;//Linguagem Open-Source
+        ?>
+    </body>
+</html>
